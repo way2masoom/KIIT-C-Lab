b9577748d3095cb789548a06a03b6fff1fd46b10
MD Masoom Alam <101356833+way2masoom@users.noreply.github.com>
2023-03-19 21:42:06 +0530

    just added int befor s in the formula line

--- day-02/lab-Assigment/04_area_of_triangle.c
+++ day-02/lab-Assigment/04_area_of_triangle.c
@@ -4,27 +4,28 @@
 
 int main()
 {
+    
     int x, y, z; // Side of Triangle
     float area;
 
     // taking input from user
-    printf("Enter First sides of triangle:\n");
+    printf("Enter the First sides of triangle:\n");
     scanf("%d", &x);
 
-    printf("Enter three Second of triangle:\n");
+    printf("Enter the Second of triangle:\n");
     scanf("%d", &y);
 
-    printf("Enter three Third of triangle:\n");
+    printf("Enter the Third of triangle:\n");
     scanf("%d", &z);
 
-    int s; // s=perimeter
+    // int s; // s=perimeter
 
     // Calculation to find the perimeter of Triangle
-    s = (x + y + z) / 2;
+   int  s = (x + y + z) / 2;
 
     printf("perimeter of Triangle is = %d", s);
 
-    // Calculation to find the are of Triangle
+    // Calculation to find the area of Triangle
     area = sqrt(s * (s - x) * (s - y) * (s - z));
     printf("\narea of triangle is %f", area);
 
