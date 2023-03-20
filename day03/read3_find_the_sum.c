4267104619f0215497cc9579c84298dff173a8df
MD Masoom Alam <101356833+way2masoom@users.noreply.github.com>
2023-03-20 16:40:11 +0530

    day3

---
+++ day03/read3_find_the_sum.c
@@ -0,0 +1,19 @@
+// read a 3_Digits number and find sum of 3-digit
+#include <stdio.h>
+int main()
+{
+    int num, a, b, c, d;
+    // Tacking Input as 237
+    printf("Enter the theree digits Number:");
+    scanf("%d", &num);
+    a = num % 10; // Remender will disply [ex=237] then only 7 will disply
+    b = num / 10; // Quotient will disaply [ex=23]
+    c = b / 10;   // It will display 2 [ex=23/10]=2
+    d = b % 10;   // It will disply 3 [ex=23%10]=2
+
+    int sum = a + c + d;
+
+    printf("The sum of 3 digits number is = %d", sum);
+
+    return 0;
+}
