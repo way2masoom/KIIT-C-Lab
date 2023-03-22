c5875d0075a788d9baf2298a8027b0ccb998a60b
MD Masoom Alam <101356833+way2masoom@users.noreply.github.com>
2023-03-22 15:39:20 +0530

    Program to check greater numbe among two number

---
+++ day04/Samplequestion/01_largest_num.c
@@ -0,0 +1,20 @@
+// WAP to find the largest between two number
+#include <stdio.h>
+int main()
+{
+    int num1, num2;
+    printf("Enter the First Number:");
+    scanf("%d", &num1);
+
+    printf("Enter the First Number:");
+    scanf("%d", &num1);
+
+    if (num1 > num2)
+
+        printf("Num1 is greater");
+
+    else
+        printf("Num2 is greater");
+
+    return 0;
+}
