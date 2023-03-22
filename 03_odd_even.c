c175db1aa7e436919bbec3ae9a43286dae73c4e8
MD Masoom Alam <101356833+way2masoom@users.noreply.github.com>
2023-03-22 16:52:25 +0530

    odd or even

---
+++ 03_odd_even.c
@@ -0,0 +1,18 @@
+// WnumP to check whenumther the given progrnumm to ODD or Even
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Enetr numny Number Please:");
+    scanf("%d", &num);
+
+    if (num % 2 == 0)
+
+        printf("Even Number");
+
+    else
+
+        printf("ODD Number");
+
+    return 0;
+}
