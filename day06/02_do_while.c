3e6ffe2daf8313e0e6bd5979f7cf0b433ba9e95d
MD Masoom Alam <101356833+way2masoom@users.noreply.github.com>
2023-04-12 16:43:39 +0530

    while LOOP and Do while Loop

---
+++ day06/02_do_while.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    // While LOOP
+    int i;
+    i = 1;
+    do
+    {
+        printf("\n KIIT");
+        i++;
+    } while (1 < 5);
+
+}
---
+++ day06/02_loop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    // While LOOP
+    int i;
+    i=1;
+    while(i)
+    {
+       printf("%d \n",i);
+
+       i--;
+    }
+    printf("Thank You!!");
+    
+}
